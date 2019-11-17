799d2f41cefff9076f59ee20de4a0aaf24e1ebed
Igor <kuzmin-igor@bk.ru>
2019-11-17 21:01:29 +0200

    Настроил плагины для работы с модулем common

---
+++ cloud-publishing-common/cloud-publishing-dao/src/main/java/com/ikuzmin/cloud/publishing/configs/AbstractPublishingDataBaseContext.java
@@ -0,0 +1,55 @@
+package com.ikuzmin.cloud.publishing.configs;
+
+import java.util.Properties;
+import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+
+
+/**
+ *
+ * @author Igor Kuzmin
+ */
+
+public class AbstractPublishingDataBaseContext {
+
+  @Bean
+  public DataSource basicDataSource() {
+    BasicDataSource dataSource = new BasicDataSource();
+    dataSource.setUrl("jdbc:mysql://localhost:3306/cloud-publishing?serverTimezone=UTC");
+    dataSource.setUsername("root");
+    dataSource.setPassword("root");
+    dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+    dataSource.setInitialSize(5);
+    dataSource.setMaxTotal(20);
+    return dataSource;
+  }
+  
+  @Bean
+  public LocalSessionFactoryBean sessionFactoryBean() {
+    LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+    sessionFactoryBean.setDataSource(basicDataSource());
+    sessionFactoryBean.setPackagesToScan("com.ikuzmin.cloud.publishing.entities");
+    sessionFactoryBean.setHibernateProperties(hibernateProperties());
+    return sessionFactoryBean;
+  }
+  
+  @Bean
+  public PlatformTransactionManager transactionManager() {
+    HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+    transactionManager.setSessionFactory(sessionFactoryBean().getObject());
+    return transactionManager;
+  }
+  
+  private Properties hibernateProperties() {
+    Properties properties = new Properties();
+    properties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+    properties.put("hibernate.show_sql", "true");
+    properties.put("hibernate.format_sql", "true");
+    return properties;
+  }
+  
+}
---
+++ cloud-publishing-common/cloud-publishing-dao/src/main/java/com/ikuzmin/cloud/publishing/entities/Employee.java
@@ -0,0 +1,58 @@
+package com.ikuzmin.cloud.publishing.entities;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Data;
+
+/**
+ *
+ * @author Igor Kuzmin
+ */
+
+@Data
+@Entity
+public class Employee implements Serializable {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column
+  private Integer id;
+  
+  @Column(nullable = false)
+  private String firstName;
+  
+  @Column(nullable = false)
+  private String lastName;
+  
+  @Column
+  private String middleName;
+  
+  @Column(nullable = false, unique = true)
+  private String email;
+  
+  @Column(nullable = false)
+  private String password;
+  
+  @Column(nullable = false)
+  private Character sex;
+  
+  @Column(nullable = false)
+  private Short birthYear;
+  
+  @Column(nullable = false)
+  private String address;
+  
+  @Column(nullable = false)
+  private Character type;
+  
+  @Column(nullable = true)
+  private Integer educationId;
+  
+  @Column(nullable = false)
+  private Boolean chiefEditor;
+  
+}
