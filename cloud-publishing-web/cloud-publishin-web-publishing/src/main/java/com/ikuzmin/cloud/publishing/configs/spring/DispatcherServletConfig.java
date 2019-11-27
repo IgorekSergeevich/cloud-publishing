a99a4db167e09e5ad15167b5e672e778e5cb26e2
Igor <kuzmin-igor@bk.ru>
2019-11-27 17:03:27 +0300

    Добавил разрешение на использование папки со статическими ресурсами

--- cloud-publishing-web/cloud-publishin-web-publishing/src/main/java/com/ikuzmin/cloud/publishing/configs/spring/DispatcherServletConfig.java
+++ cloud-publishing-web/cloud-publishin-web-publishing/src/main/java/com/ikuzmin/cloud/publishing/configs/spring/DispatcherServletConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
@@ -25,6 +26,12 @@ public class DispatcherServletConfig implements WebMvcConfigurer {
     bean.setPrefix("WEB-INF/view/");
     bean.setSuffix(".jsp");
     return bean;
+  }
+  
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/resources/**")
+      .addResourceLocations("/resources/");
   }
 
 }
--- cloud-publishing-web/cloud-publishin-web-publishing/src/main/java/com/ikuzmin/cloud/publishing/configs/spring/SpringSecurityConfig.java
+++ cloud-publishing-web/cloud-publishin-web-publishing/src/main/java/com/ikuzmin/cloud/publishing/configs/spring/SpringSecurityConfig.java
@@ -34,6 +34,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests()
             .antMatchers("/main").hasAnyAuthority("ADMIN")
+            .antMatchers("/resources/**").permitAll()
             .anyRequest().authenticated()
             .and()
             .formLogin().permitAll().loginPage("/login").failureUrl("/login?err")
