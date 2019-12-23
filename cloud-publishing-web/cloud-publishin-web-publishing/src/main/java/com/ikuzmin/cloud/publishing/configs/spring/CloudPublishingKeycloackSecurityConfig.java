f7917543556b7295ac4898e76c09c78cb1b3664c
Igor <kuzmin-igor@bk.ru>
2019-12-23 16:08:37 +0300

    Keycloack bootsraping. It is necessary to find a solution to the problem with keycloack work.

---
+++ cloud-publishing-web/cloud-publishin-web-publishing/src/main/java/com/ikuzmin/cloud/publishing/configs/spring/CloudPublishingKeycloackSecurityConfig.java
@@ -0,0 +1,58 @@
+package com.ikuzmin.cloud.publishing.configs.spring;
+
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.keycloak.adapters.springsecurity.management.HttpSessionManager;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+/**
+ *
+ * @author Igor Kuzmin
+ */
+@Profile("keycloak")
+@KeycloakConfiguration
+public class CloudPublishingKeycloackSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    KeycloakAuthenticationProvider provider = new KeycloakAuthenticationProvider();
+    provider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+    auth.authenticationProvider(provider);
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests()
+            .antMatchers("/main").hasAnyAuthority("ADMIN")
+            .antMatchers("/resources/**").permitAll()
+            .anyRequest().authenticated();
+  }
+  
+  @Override
+  protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+    return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+  }
+  
+  @Bean
+  public KeycloakSpringBootConfigResolver keycloakConfigResolver() {
+    return new KeycloakSpringBootConfigResolver();
+  }
+    
+  @Bean
+  @Override
+  @ConditionalOnMissingBean(HttpSessionManager.class)
+  protected HttpSessionManager httpSessionManager() {
+    return new HttpSessionManager();
+  }
+  
+}
--- cloud-publishing-web/cloud-publishin-web-publishing/src/main/java/com/ikuzmin/cloud/publishing/configs/spring/SpringSecurityConfig.java
+++ cloud-publishing-web/cloud-publishin-web-publishing/src/main/java/com/ikuzmin/cloud/publishing/configs/spring/SpringSecurityConfig.java
@@ -3,7 +3,7 @@ package com.ikuzmin.cloud.publishing.configs.spring;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,6 +15,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
  *
  * @author Igor Kuzmin
  */
+@Profile({"development", "production"})
 @EnableWebSecurity
 @ComponentScan("com.ikuzmin.cloud.publishing.configs.security")
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
